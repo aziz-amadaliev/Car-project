437fc3d704e0fc77d0e74d14bb810e74b5bad6c1
Aziz <aziz.amadaliev@gmail.com>
2022-01-10 12:13:19 -0500

    changes in all files

---
+++ single-car.js
@@ -0,0 +1,10 @@
+import {filteredCar} from "./helpers.js"
+//import {processClick} from "./main.js"
+//import {searchedCarBrand} from "./main.js"
+
+const filteredCarData = filteredCar();
+console.log(filteredCarData)
+console.log("hi")
+//console.log(searchedCarBrand)
+
+const carInfo = document.querySelector(".car-info")
