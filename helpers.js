d9606b327dfa9d205d5bd469fa296b9663409fd3
Venera <ven.abdyganyeva@gmail.com>
2022-01-10 13:53:16 +0600

    dropdown for body, color and engine

--- helpers.js
+++ helpers.js
@@ -9,8 +9,9 @@ export const filteredCar = (searchedCarBrand) => {
         const carName = carObj.strCarBrand.toLowerCase();
         //console.log(carName, 'line16')
         return carName.includes(searchedCarBrand);
-        
     } )
     //console.log(typeof filteredData, 'line19')
     return filteredData;
 }
+
+
--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const container = document.querySelector(".container");
 const button = document.querySelector(".btn-search");
 const brand = document.querySelector(".brand");
 
+
 const processClick = (event) => {
     event.preventDefault()
 
@@ -32,9 +33,85 @@ const processClick = (event) => {
 
 button.addEventListener("click", processClick)
 
-/* const openImage = () => {
-    console.log(filteredCarByBrand[1], 'line36')
+
+
+
+ /* Venera */
+ //BODY STYLE
+const selectBody = document.querySelector("#selectBody");
+
+selectBody.addEventListener('change', showFilteredCarBody)
+
+function showFilteredCarBody (event){
+    const choice = selectBody.value;
+    //console.log(choice, 'line48')
+    const filteredCarBody = data.filter((carObj) => {
+        //console.log(carObj.strCarBody)
+        const carBodyStyle = carObj.strCarBody.toLowerCase();
+        const carImage = carObj.strCarImage;
+        //console.log(carBodyStyle.length, 'line53')
+        if(carBodyStyle === choice){
+            const newDiv = document.createElement("div");
+            newDiv.innerHTML = carObj.strCarBody;
+            document.body.appendChild(newDiv)
+
+            const img = document.createElement("img");
+            img.src = carObj.strCarImage;
+            document.body.appendChild(img);
+            img.style.width = "250px";
+        }
+    }); 
 }
 
-image.addEventListener("click", openImage) */
+//COLOR
 
+const carColor = document.querySelector("#color");
+
+carColor.addEventListener('change', showFilteredCarColor)
+
+function showFilteredCarColor (event){
+    const colorChoice = carColor.value;
+    //console.log(colorChoice);
+    const filteredCarColor = data.filter((carObj) => {
+        
+        const carBodyColor = carObj.strCarColor.toLowerCase();
+        
+        if(carBodyColor === colorChoice){
+            const newDiv = document.createElement("div");
+            newDiv.innerHTML = carBodyColor;
+            document.body.appendChild(newDiv)
+
+            const img = document.createElement("img");
+            img.src = carObj.strCarImage;
+            document.body.appendChild(img);
+            img.style.width = "250px";
+        }
+    }); 
+} 
+
+//ENGINE
+
+const selectEngine = document.querySelector("#engine");
+
+selectEngine.addEventListener('change', showFilteredCarEngine)
+
+function showFilteredCarEngine (event){
+    const engineChoice = selectEngine.value;
+    
+    const filteredCarEngine = data.filter((carObj) => {
+        
+        const carEngine = carObj.strCarEngine.toLowerCase();
+        const carImage = carObj.strCarImage;
+        
+        if(carEngine === engineChoice){
+            const newDiv = document.createElement("div");
+            newDiv.innerHTML = carObj.strCarEngine;
+            document.body.appendChild(newDiv)
+
+            const img = document.createElement("img");
+            img.src = carObj.strCarImage;
+            document.body.appendChild(img);
+            img.style.width = "250px";  
+        }
+    });   
+}
