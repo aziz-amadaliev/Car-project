78756e0dda12e4f10bbc1eefb07b2d4802229488
Aziz <aziz.amadaliev@gmail.com>
2022-01-10 12:12:44 -0500

    changes in index html, style.css and all js files

--- main.js
+++ main.js
@@ -3,38 +3,74 @@ import {filteredCar} from "./helpers.js";
 
 const data = getCarData();
 
-const container = document.querySelector(".container");
+const mainSection = document.querySelector(".main-section");
+const carSection = document.querySelector(".car-section");
 const button = document.querySelector(".btn-search");
 const brand = document.querySelector(".brand");
 
-const processClick = (event) => {
+
+window.addEventListener('load', (event) => {
+    for (let i=0; i < data.length; i++) {
+        const div = document.createElement("div")
+        div.classList.add("myDiv")
+
+        const paragraph = document.createElement("p");
+        paragraph.innerText = data[i].strCarBrand;
+        div.appendChild(paragraph);
+
+        const aTag = document.createElement("a")
+        aTag.href = "./car.html";
+        aTag.target = "_blank";
+        const image = document.createElement("img");
+        image.src = data[i].strCarImage;
+        image.style.width = "400px";
+        aTag.appendChild(image);
+        div.appendChild(aTag);
+        
+        carSection.appendChild(div);
+    }
+
+});
+
+export const processClick = (event) => {
     event.preventDefault()
 
+    while (mainSection.firstChild) {
+        mainSection.removeChild(mainSection.firstChild); //to remove child elements when search button clicked
+    }
+
     const searchedCarBrand = brand.value.toLowerCase();
+    //console.log(searchedCarBrand, 'line44')
 
     const filteredCarByBrand = filteredCar(searchedCarBrand);
-    console.log(filteredCarByBrand, 'line32')
-    console.log(filteredCarByBrand[0].strCarBrand, 'line33')
+    //console.log(filteredCarByBrand, 'line32')
+
 
     for (let i=0; i < filteredCarByBrand.length; i++) {
+        const div = document.createElement("div")
+        div.classList.add("myDiv")
+
         const paragraph = document.createElement("p");
+        //console.log(paragraph.classList)
         paragraph.innerText = filteredCarByBrand[i].strCarBrand;
-        container.appendChild(paragraph);
+        paragraph.classList.add("car-text")
+        div.appendChild(paragraph);
 
         const image = document.createElement("img");
+        const aTag = document.createElement("a")
+        aTag.href = "./car.html";
+        aTag.target = "_blank";
+
         image.src = filteredCarByBrand[i].strCarImage;
-        container.appendChild(image);
-        image.style.width = "300px"
-        //image.style.display = "inline-block"
+        image.classList.add("car-image")
+        image.style.width = "500px";
+        aTag.appendChild(image);
+        div.appendChild(aTag)
+
+        mainSection.appendChild(div);
+       
     }
 
 }
 
 button.addEventListener("click", processClick)
-
-/* const openImage = () => {
-    console.log(filteredCarByBrand[1], 'line36')
-}
-
-image.addEventListener("click", openImage) */
-
