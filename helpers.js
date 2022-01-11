6e2a0f633e82acd9992c913374b6ec8b37822757
kanykej <k.boron93@gmail.com>
2022-01-11 23:46:11 +0600

    commit

--- helpers.js
+++ helpers.js
@@ -1,16 +1,39 @@
-import { getCarData } from "./car-data.js";
+//Gearbox
+const gearbox = document.querySelector("#gearbox");
+gearbox.addEventListener('change', showFilteredCarGearbox)
 
-
-export const filteredCar = (searchedCarBrand) => {
-
-    const data = getCarData();
-    const filteredData = data.filter((carObj) => {
-        //console.log(carObj.strCarBrand)
-        const carName = carObj.strCarBrand.toLowerCase();
-        //console.log(carName, 'line16')
-        return carName.includes(searchedCarBrand);
+function showFilteredCarGearbox (event){
+    while (mainSection.firstChild) {
+        mainSection.removeChild(mainSection.firstChild); //to remove child elements when search button clicked
+    }
+    const gearboxChoice = gearbox.value;
+    
+    const filteredCarGearbox = data.filter((carObj) => {
+        const carGearbox = carObj.strCarGearbox.toLowerCase();
+        const carImage = carObj.strCarImage;
         
-    } )
-    //console.log(typeof filteredData, 'line19')
-    return filteredData;
+        if(carGearbox === gearboxChoice){
+            const div = document.createElement("div")
+            div.classList.add("myDiv")
+    
+            const paragraph = document.createElement("p");
+            //console.log(paragraph.classList)
+            paragraph.innerText = carObj.strCarBrand;
+            paragraph.classList.add("car-text")
+            div.appendChild(paragraph);
+    
+            const image = document.createElement("img");
+            const aTag = document.createElement("a")
+            aTag.href = "./car.html";
+            aTag.target = "_blank";
+    
+            image.src = carObj.strCarImage;
+            image.classList.add("car-image")
+            image.style.width = "500px";
+            aTag.appendChild(image);
+            div.appendChild(aTag)
+    
+            mainSection.appendChild(div); 
+        }
+    });   
 }
