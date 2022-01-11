a21c8c37f2e697ba60dacb51146f0a08ebc6bcaa
Platon0102 <nursultan.abdyrazakov1@gmail.com>
2022-01-11 16:45:02 +0600

    added filtering brand-price-year

--- helpers.js
+++ helpers.js
@@ -8,13 +8,14 @@ const yearB = document.querySelector(".yearB");
 const yearA = document.querySelector(".yearA");
 
 
-export const filteredCar = (searchedCarBrand) => {
+export const filteredCar = (searchedCarType) => {
     const data = getCarData();
     const filteredData = data.filter((carObj) => {
         //console.log(carObj.strCarBrand)
         const carName = carObj.strCarBrand.toLowerCase();
-        //console.log(carName, 'line16')
-        return carName.includes(searchedCarBrand);
+        const carPrice = carObj.numCarPriceUSD.toString()
+        const carYear = carObj.strCarYear;
+        return carName.includes(searchedCarType) ||  carYear.includes(searchedCarType) || carPrice.includes(searchedCarType)
         
     } )
     //console.log(typeof filteredData, 'line19')
@@ -26,7 +27,51 @@ function pushToHTML () {
     const filteredCarByBrand = filteredCar(searchedCarValue);
 
     for (let i=0; i < filteredCarByBrand.length; i++) {
-        if(priceB.value <= filteredCarByBrand[i].numCarPriceUSD && priceA.value >= filteredCarByBrand[i].numCarPriceUSD){
+        if((priceB.value <= filteredCarByBrand[i].numCarPriceUSD && priceA.value >= filteredCarByBrand[i].numCarPriceUSD)){
+            let paragraph = document.createElement("p");
+            paragraph.innerText = filteredCarByBrand[i].strCarBrand;
+            container.appendChild(paragraph);
+            let model = document.createElement("p");
+            model.innerText = filteredCarByBrand[i].numCarPriceUSD;
+            container.appendChild(model);
+            let year = document.createElement("p");
+            year.innerText = filteredCarByBrand[i].strCarYear;
+            container.appendChild(year);
+            let image = document.createElement("img");
+            image.src = filteredCarByBrand[i].strCarImage;
+            container.appendChild(image);
+            image.style.width = "300px"
+} } }
+
+
+function pushToHtmlYear () {
+    const searchedCarValue = brand.value.toLowerCase();
+    const filteredCarByBrand = filteredCar(searchedCarValue);
+
+    for (let i=0; i < filteredCarByBrand.length; i++) {
+        if((yearB.value <= filteredCarByBrand[i].strCarYear && yearA.value >= filteredCarByBrand[i].strCarYear) && (priceB.value <= filteredCarByBrand[i].numCarPriceUSD && priceA.value >= filteredCarByBrand[i].numCarPriceUSD)){
+            let paragraph = document.createElement("p");
+            paragraph.innerText = filteredCarByBrand[i].strCarBrand;
+            container.appendChild(paragraph);
+            let model = document.createElement("p");
+            model.innerText = filteredCarByBrand[i].numCarPriceUSD;
+            container.appendChild(model);
+            let year = document.createElement("p");
+            year.innerText = filteredCarByBrand[i].strCarYear;
+            container.appendChild(year);
+            let image = document.createElement("img");
+            image.src = filteredCarByBrand[i].strCarImage;
+            container.appendChild(image);
+            image.style.width = "300px"
+} } }
+
+
+function pushToHtmlYear1 () {
+    const searchedCarValue = brand.value.toLowerCase();
+    const filteredCarByBrand = filteredCar(searchedCarValue);
+
+    for (let i=0; i < filteredCarByBrand.length; i++) {
+        if(yearB.value <= filteredCarByBrand[i].strCarYear && yearA.value >= filteredCarByBrand[i].strCarYear){
             let paragraph = document.createElement("p");
             paragraph.innerText = filteredCarByBrand[i].strCarBrand;
             container.appendChild(paragraph);
@@ -63,12 +108,12 @@ function pushToHtmlBrand () {
     }
 }
 
-function pushToHtmlYear () {
+function pushToHTML1 () {
     const searchedCarValue = brand.value.toLowerCase();
     const filteredCarByBrand = filteredCar(searchedCarValue);
 
     for (let i=0; i < filteredCarByBrand.length; i++) {
-        if((yearB.value <= filteredCarByBrand[i].strCarYear && yearA.value >= filteredCarByBrand[i].strCarYear) || (priceB.value <= filteredCarByBrand[i].numCarPriceUSD && priceA.value >= filteredCarByBrand[i].numCarPriceUSD)){
+        if((priceB.value <= filteredCarByBrand[i].numCarPriceUSD && priceA.value >= filteredCarByBrand[i].numCarPriceUSD) && (yearB.value <= filteredCarByBrand[i].strCarYear && yearA.value >= filteredCarByBrand[i].strCarYear)){
             let paragraph = document.createElement("p");
             paragraph.innerText = filteredCarByBrand[i].strCarBrand;
             container.appendChild(paragraph);
@@ -85,7 +130,8 @@ function pushToHtmlYear () {
 } } }
 
 
-
+export { pushToHTML1 }
 export { pushToHTML };
 export { pushToHtmlBrand };
 export { pushToHtmlYear };
+export { pushToHtmlYear1 };
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import { pushToHTML } from "./helpers.js";
 import { pushToHtmlBrand } from "./helpers.js";
 import { pushToHtmlYear } from "./helpers.js";
-
-
+import { pushToHtmlYear1 } from "./helpers.js";
+import { pushToHTML1 } from "./helpers.js";
+const brand = document.querySelector(".brand");
 const button = document.querySelector(".btn-search");
 const priceB = document.querySelector(".priceB");
 const priceA = document.querySelector(".priceA");
@@ -12,13 +13,19 @@ const yearA = document.querySelector(".yearA");
 const processClick = (event) => {
     event.preventDefault()
 
-        if (priceB.value != "" && priceA.value != "") {
-            return pushToHTML ();
-        }else if (yearB.value != "" && yearA.value != "") {
-            return pushToHtmlYear ();
+        if (brand.value != "" && priceB.value == "" && priceA.value == "" && yearB.value == "" && yearA.value == ""){
+            return pushToHtmlBrand();
+        }else if (brand.value != "" && priceB.value !== "" && priceA.value !== "" && yearB.value == "" && yearA.value == "") {
+            return pushToHTML();
+        } else if (brand.value == "" && priceB.value !== "" && priceA.value !== "" && yearB.value !== "" && yearA.value !== ""){
+            return pushToHtmlYear();
+        } else if (brand.value == "" && priceB.value !== "" && priceA.value !== "" && yearB.value == "" && yearA.value == "") {
+            return pushToHTML();
+        } else if (priceB.value !== "" && priceA.value !== "" && brand.value != "" &&yearB.value !== "" && yearA.value !== "") {
+            return pushToHTML1();
         } else {
-            return pushToHtmlBrand ();
-          }
+            return pushToHtmlYear1();
+        }
 }
 
 button.addEventListener("click", processClick);
