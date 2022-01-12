cb2e3f5602b70d860f5d2a7fea699a7313b48663
Platon0102 <nursultan.abdyrazakov1@gmail.com>
2022-01-12 23:56:02 +0600

    added main page

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { pushToHtmlYear1 } from "./helpers.js";
 import { pushToHTML1 } from "./helpers.js";
 import { pushToHtmlB } from "./helpers.js";
 import { pushToHtmlA } from "./helpers.js";
+import { getCarData } from "./car-data.js";
 
 const mainSection = document.querySelector(".main-section");
 const brand = document.querySelector(".brand");
@@ -13,6 +14,32 @@ const priceB = document.querySelector(".priceB");
 const priceA = document.querySelector(".priceA");
 const yearB = document.querySelector(".yearB");
 const yearA = document.querySelector(".yearA");
+const carSection = document.querySelector(".car-section");
+const data = getCarData()
+
+window.addEventListener('load', (event) => {
+    for (let i=0; i < data.length; i++) {
+        const div = document.createElement("div")
+        div.classList.add("myDiv")
+
+        const paragraph = document.createElement("p");
+        paragraph.innerText = data[i].strCarBrand;
+        div.appendChild(paragraph);
+
+        const aTag = document.createElement("a")
+        aTag.href = "./car.html";
+        aTag.target = "_blank";
+        const image = document.createElement("img");
+        image.src = data[i].strCarImage;
+        image.style.width = "400px";
+        aTag.appendChild(image);
+        div.appendChild(aTag);
+        carSection.appendChild(div);
+    }
+
+});
+
+console.log(data)
 
 const processClick = (event) => {
     while (mainSection.firstChild) {
